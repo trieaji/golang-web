a0884c568c7271a1e4e1b558e50a3cdf8246901f
Laksa Bayu <laksabayu18.com>
2023-12-05 23:56:55 +0700

    add comment

--- middleware_test.go
+++ middleware_test.go
@@ -6,11 +6,18 @@ import (
 	"testing"
 )
 
+/* 
+1. Middleware adalah sebuah fitur dimana kita bisa menambahkan kode sebelum dan setelah sebuah handler di eksekusi
+2. Kadang middleware juga bisa digunakan untuk melakukan error handler
+3. Hal ini sehingga jika terjadi panic di Handler, kita bisa melakukan recover di middleware, dan mengubah panic tersebut menjadi panic error response
+4. Dengan ini, kita bisa menjaga aplikasi kita tidak berhenti berjalan
+*/
+
 type LogMiddleware struct {
 	Handler http.Handler
 }
 
-func(middleware *LogMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+func(middleware *LogMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {//adanya pointer di "*LogMiddleware" supaya datanya tidak diduplikat terus-menerus
 	fmt.Println("before execute handler")
 	middleware.Handler.ServeHTTP(writer, request)
 	fmt.Println("after execute handler")
@@ -21,7 +28,7 @@ type ErrorHandler struct {
 }
 
 func (errorHandler *ErrorHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	defer func() {
+	defer func() { //defer adalah function yg bisa kita jadwalkan untuk dieksekusi setelah sebuah function selesai di eksekusi, defer function akan selalu di eksekusi walaupun terjadi error di function yg di eksekusi
 		err := recover()
 		if err != nil {
 			fmt.Println("Terjadi error")
@@ -50,7 +57,7 @@ func TestMiddleware(t *testing.T) {
 
 	//error handler
 	errorHandler := &ErrorHandler{
-		Handler: &logMiddleware,
+		Handler: &logMiddleware,//diberi pointer(&) karena "LogMiddleware" nya adalah function Handler
 	}
 
 	server := http.Server{
